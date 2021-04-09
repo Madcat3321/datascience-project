5137f76c078dc4a2d0c3b7103b1b561f4a589627
DESKTOP-ODLUII5\Calvin <tanghoyan79@gmail.com>
2021-04-09 20:16:12 +0800

    new graphs

--- untitled1.py
+++ untitled1.py
@@ -1,17 +1,32 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
-header = ["Year","Quarter","Primary and below","Lower secondary","Upper secondary",
-         "Post-secondary","Post-secondary : Diploma / Certificate","Post-secondary : Sub-degree",
-         "Post-secondary : Degree","overall"]
+from pandas.plotting import lag_plot
 
+header = ["Year", "Quarter", "Primary and below", "Lower secondary", "Upper secondary",
+          "Post-secondary", "Post-secondary : Diploma / Certificate", "Post-secondary : Sub-degree",
+          "Post-secondary : Degree", "overall"]
 
-df = pd.read_excel("income.xlsx", sheet_name=0,skiprows=6,header=None)
-df = df.dropna(axis=1,how='all')
+df = pd.read_excel("income.xlsx", sheet_name=0, skiprows=6, header=None)
+df = df.dropna(axis=1, how='all')
 df.columns = header
-databyyear = df[np.isnan(df['Quarter'])] #Quarter is empty
+databyyear = df[np.isnan(df['Quarter'])]  # Quarter is empty
 databyyear = databyyear.set_index('Year')
-databyyear.drop(['Quarter'], axis=1,inplace=True)
+databyyear.drop(['Quarter'], axis=1, inplace=True)
+
+
 databyyear.plot()
-    
+plt.title(" Median monthly employment earnings of employed persons by educational attainment") # title
+plt.ylabel("Median monthly employment earnings") # y label
+plt.xlabel("Year") # x label
+
+plt.rcParams['figure.figsize'] = (16.0, 12.0)
+plt.savefig('line.png')
+plt.show()
+plt.clf()
+
+lag_plot(databyyear)
+plt.rcParams['figure.figsize'] = (16.0, 12.0)
+plt.savefig('dot.png')
+plt.show()
+
